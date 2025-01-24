a7fae2452a523ffc6c1f0a7b5afc6c890158b8bb
maakbarofficial <maakbarofficial@gmail.com>
2025-01-24 19:42:30 +0500

    Fix Theme Toggler, Fix UI of Search Input in DataTable

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,12 @@ function App() {
   }
 
   return (
-    <div className={`${theme}`}>
+    <div className={`${theme} ${theme === "light" ? "bg-white" : "bg-black"} h-screen`}>
       <div className="flex justify-center items-center p-2">
-        <button onClick={toggleTheme} className="cursor-pointer">{theme === "light" ? <FaSun /> : <FaMoon />}</button>
+        <button onClick={toggleTheme} className="cursor-pointer flex justify-center items-center gap-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">{theme === "light" ? <FaSun /> : <FaMoon />} Toggle Theme</button>
+      </div>
+      <div className="flex justify-center items-center p-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+        <h1>React Tailwind CSS Custom DataTable</h1>
       </div>
       <div className="p-5">
         <DataTable data={data} searchBar={true} pagination={true} removableRows={true} excelExport={true} />
--- src/DataTable.jsx
+++ src/DataTable.jsx
@@ -126,16 +126,43 @@ const DataTable = ({
         <div className="max-w-full overflow-x-auto py-5">
             <div className="flex w-full items-center justify-between mb-5">
                 {searchBar ? (
-                    <div className="flex md:w-[30%] w-[70%] items-center gap-5 rounded-lg px-3 py-2 bg-[#303030]">
-                        <SearchOutline className={"!text-gray-300"} />
-                        <input
-                            type="text"
-                            placeholder="Search"
-                            value={searchTerm}
-                            onChange={handleSearch}
-                            className="w-full outline-none bg-transparent"
-                        />
+                    <div className="max-w-sm">
+                        <label
+                            htmlFor="default-search"
+                            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+                        >
+                            Search
+                        </label>
+                        <div className="relative">
+                            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                                <svg
+                                    className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 20 20"
+                                >
+                                    <path
+                                        stroke="currentColor"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth={2}
+                                        d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                                    />
+                                </svg>
+                            </div>
+                            <input
+                                value={searchTerm}
+                                onChange={handleSearch}
+                                type="search"
+                                id="default-search"
+                                className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="Search..."
+                                required=""
+                            />
+                        </div>
                     </div>
+
                 ) : (
                     <div></div>
                 )}
