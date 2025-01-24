d195f44f886b0af857d71bab20271b32450dd645
maakbarofficial <maakbarofficial@gmail.com>
2025-01-24 18:41:08 +0500

    Apply Dark Mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react"
+
 function App() {
+  const [theme, setTheme] = useState('light')
+
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light')
+  }
+
   return (
-    <>
-      <h1 class="text-3xl font-bold underline">
+    <div className={`${theme}`}>
+      <button onClick={toggleTheme} className="bg-blue-500 text-white p-2 rounded-md cursor-pointer">Theme Toggle</button>
+      <h1 class="text-3xl font-bold underline bg-white dark:bg-black dark:text-white">
         Hello world!
       </h1>
-    </>
+    </div>
   )
 }
 
